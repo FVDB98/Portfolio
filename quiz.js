12c45335b8ef529d17cf0d59a4fa42930b006043
FVDB98 <felixvanderborgh@gmail.com>
2023-01-11 14:48:45 +0000

    updates and quiz added

---
+++ quiz.js
@@ -0,0 +1,87 @@
+function Quiz(questions) {
+    this.score = 0;
+    this.questions = questions;
+    this.currentQuestionIndex = 0;
+}
+
+Quiz.prototype.guess = function(answer) {
+    if(this.getCurrentQuestion().isCorrectAnswer(answer)) {
+        this.score++;
+    }
+    this.currentQuestionIndex++;
+};
+
+Quiz.prototype.getCurrentQuestion = function() {
+    return this.questions[this.currentQuestionIndex];
+};
+
+Quiz.prototype.hasEnded = function() {
+    return this.currentQuestionIndex >= this.questions.length;
+};
+function Question(text, choices, answer) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+}
+
+Question.prototype.isCorrectAnswer = function (choice) {
+    return this.answer === choice;
+};
+var QuizUI = {
+    displayNext: function () {
+        if (quiz.hasEnded()) {
+            this.displayScore();
+        } else {
+            this.displayQuestion();
+            this.displayChoices();
+            this.displayProgress();
+        }
+    },
+    displayQuestion: function() {
+        this.populateIdWithHTML("question", quiz.getCurrentQuestion().text);
+    },
+    displayChoices: function() {
+        var choices = quiz.getCurrentQuestion().choices;
+
+        for(var i = 0; i < choices.length; i++) {
+            this.populateIdWithHTML("choice" + i, choices[i]);
+            this.guessHandler("guess" + i, choices[i]);
+        }
+    },
+    displayScore: function() {
+        var gameOverHTML = "<h1>Thanks for Playing!</h1>";
+        gameOverHTML += "<h2> Your score is: " + quiz.score + " / 5</h2>";
+        this.populateIdWithHTML("quiz", gameOverHTML);
+    },
+    
+    populateIdWithHTML: function(id, text) {
+        var element = document.getElementById(id);
+        element.innerHTML = text;
+    },
+    guessHandler: function(id, guess) {
+        var button = document.getElementById(id);
+        button.onclick = function() {
+            quiz.guess(guess);
+            QuizUI.displayNext();
+        }
+    },
+    
+    displayProgress: function() {
+        var currentQuestionNumber = quiz.currentQuestionIndex + 1;
+        this.populateIdWithHTML("progress", "Question " + currentQuestionNumber + " of " + quiz.questions.length);
+    }
+};
+//Create Questions
+var questions = [
+    new Question("What Football team do I support?", [ "Chelsea:", "Arsenal:", "West Ham:"], "Chelsea:"),
+    new Question("What Warhammer 40k models do I collect?", ["Space Marines:","Orks:", "Chaos Space Marines:"], "Chaos Space Marines:"),
+    new Question("What was the score in the England vs Germany game I was at?", ["2-0:","2-1:", "1-0:"], "2-0:"),
+    new Question("is my Koi fish shirt nice in the picture above?", ["Yes:","No:", "Very No:"], "Yes:"),
+    new Question("What 3 sports do I enjoy", ["Rugby, Cricket, and Tennis:","Rugby, Cricket, and Football:", "Cricket, Football, and Squash:"], "Rugby, Cricket, and Football:")
+];
+
+//Create Quiz
+var quiz = new Quiz(questions);
+
+//Display Quiz
+QuizUI.displayNext();
